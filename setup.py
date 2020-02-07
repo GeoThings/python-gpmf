6a0d4e911664eee48252b39aaebe24289e9e28d5
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-02-07 13:45:12 +0800

    Try to make poetry happy :(

--- setup.py
+++ setup.py
@@ -3,17 +3,9 @@ import os.path
 import subprocess
 from distutils.core import setup
 
-git_version = 'UNKNOWN'
-try:
-    git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
-except subprocess.CalledProcessError as e:
-    # print "Got error when trying to read git version: %s" % e
-    pass
-
 setup(
     name='gpmf',
-    version='0.0.1dev-%s' % git_version,
-    # version='0.6.6',
+    version='0.7.0',
     author='Eero "rambo" af Heurlin',
     author_email='rambo@iki.fi',
     packages=['gpmf', ],
