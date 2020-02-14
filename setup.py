536dabbb059d0e2af078271c2d8503dff5dbb327
Hsiao-Ting Yu <sst.dreams@gmail.com>
2020-02-14 23:37:18 +0800

    bump python-dateutil dependency for now

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     long_description=open('README.md').read(),
     description='GoPro Metadata Format parser',
     #install_requires=open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), 'rt').readlines(),
-    install_requires=['construct==2.8.12', 'python-dateutil==2.6.1', 'hachoir==3.1.1'],
+    install_requires=['construct==2.8.12', 'python-dateutil<2.9.0', 'hachoir==3.1.1'],
     url='https://github.com/rambo/python-gpmf',
 )
